68dc45ea148e2c3e1df51fe2855893da8aa54d87
cyj <2692104788@qq.com>
2025-07-10 10:36:42 +0800

    add: add frontend

---
+++ vue/src/components/Aside.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "Aside"
+})
+</script>
+
+
+
+<style scoped>
+
+</style>
